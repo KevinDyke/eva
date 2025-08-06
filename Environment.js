65e7145b441ef99f547a638aaf8b419dfad6b520
Kevin James Dyke <kdyke058@gmail.com>
2025-08-06 14:41:31 +0100

    Added environments along with variables decleration

---
+++ Environment.js
@@ -0,0 +1,33 @@
+/**
+ * Environment: names storage.
+ */
+class Environment {
+    /**
+     * Creates an environment with given record.
+     */
+    constructor(record = {}) {
+        this.record = record;
+    }
+
+    /**
+     * Creates a varable with the given name and value.
+     */
+    define(name, value) {
+        this.record[name] = value;
+        return value;
+    }
+
+    /**
+     * Returns the value of a defined variable, or throws
+     * if the variable is not defined.
+     */
+    lookup(name) {
+        if (!this.record.hasOwnProperty(name)) {
+            throw new ReferenceError(`Variable "${name}' is not defined.`);
+        }
+        return this.record[name];
+    }
+}
+
+
+module.exports = Environment;
